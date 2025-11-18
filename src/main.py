3b4291f5b51171dc1c8937c3bd2a2b8e7fd6f47b
BigbossDD <Legojaddai@gmail.com>
2025-11-18 23:18:19 +0300

    Remove unused files and add initial training scripts for DQN, PPO, and Rainbow DQN

---
+++ src/main.py
@@ -0,0 +1,27 @@
+import argparse
+from train import train_dqn
+from train import train_ppo
+from train import train_rainbowDQN
+
+def main():
+    print("I am alive")
+    parser = argparse.ArgumentParser(description="RL Training Launcher")
+    parser.add_argument("--algo", type=str, required=True,
+                        choices=["dqn", "rainbow", "ppo"],
+                        help="Algorithm to train: dqn | rainbow | ppo")
+    args = parser.parse_args()
+
+    if args.algo == "dqn":
+        print("\n=== Training DQN ===\n")
+        train_dqn()
+
+    elif args.algo == "rainbow":
+        print("\n=== Training Rainbow DQN ===\n")
+        train_rainbowDQN()
+
+    elif args.algo == "ppo":
+        print("\n=== Training PPO ===\n")
+        train_ppo()
+
+if __name__ == "__main__":
+    main()
